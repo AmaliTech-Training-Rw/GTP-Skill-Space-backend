e5e3bccd0697ec5b741a1945a809219c2ad7a126
“Ivan-Muhumuza” <ivan.muhumuza@amalitechtraining.org>
2024-10-06 20:21:16 +0200

    feat(setup): initialize assessment-service project with initial development files

---
+++ assessment-service/src/main/java/com/skillspace/assessment/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.skillspace.assessment.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/assessments")
+    public String getUsers() {
+        return "List of assessments";
+    }
+
+}
