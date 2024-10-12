8089d808d89c04897bc8dfe73b8f4b307a33a531
“Ivan-Muhumuza” <ivan.muhumuza@amalitechtraining.org>
2024-10-12 15:26:37 +0200

    add custom query to get  pending  companies

--- user-management-service/src/main/java/com/skillspace/user/repository/CompanyRepository.java
+++ user-management-service/src/main/java/com/skillspace/user/repository/CompanyRepository.java
@@ -3,13 +3,19 @@ package com.skillspace.user.repository;
 import com.skillspace.user.entity.Account;
 import com.skillspace.user.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
     Optional<Company> findByUserId(Account account);
+
+    @Query("SELECT c FROM Company c WHERE c.userId.status = 'PENDING'")
+    List<Company> findPendingCompanies();
 }
 
 
